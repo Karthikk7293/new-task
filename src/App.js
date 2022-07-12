d80444a7b97a4432c41c2442359d109a2329e206
karthikk7292 <karthikk7293@gmail.com>
2022-07-12 19:09:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import LandingScreen from './pages/LandingScreen';
+import SingleProductScreen from './pages/SingleProductScreen'
+import ErrorScreen from './pages/ErrorScreen'
+import CartScreen from './pages/CartScreen'
+import Header from './components/header/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className='main py-5'>
+        <Routes>
+          <Route path='/' element={<LandingScreen />} />
+          <Route path='/single-product/:productId' element={<SingleProductScreen />} />
+          <Route path='/cart' element={<CartScreen />} />
+          <Route path='*' element={<ErrorScreen />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
